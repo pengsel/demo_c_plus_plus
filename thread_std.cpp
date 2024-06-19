bdc623864615b6c9b3ef15b93381ad3aee219203
pengsel <pengsel@qq.com>
2024-06-19 21:28:19 +0800

    add readme

--- thread_std.cpp
+++ thread_std.cpp
@@ -1,55 +1,55 @@
-// ��ʾ���̵߳�CPP����
-// ʹ��������ͬ�Ŀɵ��ö���
+// 演示多线程的CPP程序
+// 使用三个不同的可调用对象
 #include <iostream>
 #include <thread>
 using namespace std;
 
-// һ�����⺯��
+// 一个虚拟函数
 void foo(int Z)
 {
     for (int i = 0; i < Z; i++) {
-        cout << "�߳�ʹ�ú���ָ����Ϊ�ɵ��ò���\n";
+        cout << "线程使用函数指针作为可调用参数\n";
     }
 }
 
-// �ɵ��ö���
+// 可调用对象
 class thread_obj {
 public:
     void operator()(int x)
     {
         for (int i = 0; i < x; i++)
-            cout << "�߳�ʹ�ú���������Ϊ�ɵ��ò���\n";
+            cout << "线程使用函数对象作为可调用参数\n";
     }
 };
 
 int main()
 {
-    cout << "�߳� 1 ��2 ��3 "
-            "��������" << endl;
+    cout << "线程 1 、2 、3 "
+            "独立运行" << endl;
 
-    // ����ָ��
+    // 函数指针
     thread th1(foo, 3);
 
-    // ��������
+    // 函数对象
     thread th2(thread_obj(), 3);
 
-    // ���� Lambda ����ʽ
+    // 定义 Lambda 表达式
     auto f = [](int x) {
         for (int i = 0; i < x; i++)
-            cout << "�߳�ʹ�� lambda ����ʽ��Ϊ�ɵ��ò���\n";
+            cout << "线程使用 lambda 表达式作为可调用参数\n";
     };
 
-    // �߳�ͨ��ʹ�� lambda ����ʽ��Ϊ�ɵ��õĲ���
+    // 线程通过使用 lambda 表达式作为可调用的参数
     thread th3(f, 3);
 
-    // �ȴ��߳����
-    // �ȴ��߳� t1 ���
+    // 等待线程完成
+    // 等待线程 t1 完成
     th1.join();
 
-    // �ȴ��߳� t2 ���
+    // 等待线程 t2 完成
     th2.join();
 
-    // �ȴ��߳� t3 ���
+    // 等待线程 t3 完成
     th3.join();
 
     return 0;
